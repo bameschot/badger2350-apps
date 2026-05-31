b111ed7318108f9412af40bcb233fa2c2ebe08d7
bameschot <53991953+bameschot@users.noreply.github.com>
2026-05-31 22:00:30 +0200

    rudimentary title list

--- apps/reader/cli-ereader.py
+++ apps/reader/cli-ereader.py
@@ -13,7 +13,13 @@ def main(stdscr):
     mainWindow = newwin(mainWindowHeight, applicationWidth, 0, 0)
     mainWindow.keypad(True)
 
+    init_pair(0, COLOR_WHITE, COLOR_BLACK)
+    init_pair(1, COLOR_RED, COLOR_BLACK)
+    init_pair(2, COLOR_GREEN, COLOR_BLACK)
+
+
     statusWindow = newwin(1, applicationWidth, mainWindowHeight, 0)
+    
 
     ereader = EReader(
         "./apps/reader/books",
@@ -23,9 +29,21 @@ def main(stdscr):
     )
 
     ereader.loadBookTitles()
+
+    mainWindow.clear()
+    titleIdx = 0
+    selectedIdx = 0
+    for title in ereader.loadedTitles:
+        
+        mainWindow.addstr(titleIdx,0 , f'idx: {titleIdx:02d}; title: {title["title"]}',color_pair(1))
+        titleIdx+=1
+
+    mainWindow.refresh()
+
+    mainWindow.getkey()
+
     ereader.loadBook(0)
     ereader.resizeScreen(applicationWidth,mainWindowHeight)
-    init_pair(1, COLOR_RED, COLOR_BLACK)
     
 
     # This raises ZeroDivisionError when i == 10.
@@ -53,10 +71,10 @@ def main(stdscr):
         statusWindow.addstr(0, 0, f'[title: {title}; page: {pageIdx:04d}; chunk: {chunkIdx:04d}]',color_pair(1))
         statusWindow.refresh()
 
-        inp = mainWindow.getch()
-        if inp == KEY_RIGHT:
+        inp = mainWindow.getkey()
+        if inp == "KEY_RIGHT":
             ereader.nextPage()
-        elif inp == KEY_LEFT:
+        elif inp == "KEY_LEFT":
             ereader.previousPage()
         elif inp == 'E':
             isReadingBook = False
